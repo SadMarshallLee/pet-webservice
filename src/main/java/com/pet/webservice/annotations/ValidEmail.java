692bfa2a28e3dbc7f7772549ffc24749ead1fee0
Aleksandr <38037335+sadmarshalllee@users.noreply.github.com>
2022-05-14 20:03:21 +0300

    add two requests objects and one annotation

---
+++ src/main/java/com/pet/webservice/annotations/ValidEmail.java
@@ -0,0 +1,19 @@
+package com.pet.webservice.annotations;
+
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target({TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailValidator.class)
+public @interface ValidEmail {
+    String message() default "Invalid Email";
+
+}
--- src/main/java/com/pet/webservice/payload/request/LoginRequest.java
+++ src/main/java/com/pet/webservice/payload/request/LoginRequest.java
@@ -2,8 +2,14 @@ package com.pet.webservice.payload.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class LoginRequest {
 
+    @NotEmpty(message = "Username can't be empty")
+    private String username;
+    @NotEmpty(message = "Password can't be empty")
+    private String password;
 
 }
